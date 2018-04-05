654f6d7c6162e038494bee5338ef017b0c1e4de2
NicoMazzoconi <36174156+NicoMazzoconi@users.noreply.github.com>
2018-04-05 13:23:26 -0300

    Agregado getFloat

--- lib.h/lib.h
+++ lib.h/lib.h
@@ -2,5 +2,5 @@
 #define LIB_H_INCLUDED
 
 int getInt(char *mensaje, char *error, int intentos, int maximo, int minimo, int *resultado);
-
+float getFloat(char *mensaje, char *error, int intentos, float maximo, float minimo, float *resultado);
 #endif // LIB_H_INCLUDED
